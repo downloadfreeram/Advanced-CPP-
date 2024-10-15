e9e63486c3943ced0f778fe8595effbf7d8fcb55
Kacper R <126609474+downloadfreeram@users.noreply.github.com>
2024-10-15 21:26:17 +0200

    Update cppAdv_lista1.cpp

--- cppAdv_lista1/cppAdv_lista1.cpp
+++ cppAdv_lista1/cppAdv_lista1.cpp
@@ -1,23 +1,39 @@
-﻿#include <iostream>
+#include <iostream>
 #include "template_header.h"
+
+
 int main()
 {
+    //zad1
     auto a = 4.6;
     auto b = 5.8;
-    std::vector<int> w{23, 1, 10, 5, 2};
-    my_max(a, b, ([](auto a, auto b) -> auto {return a < b ? b : a;}));
-    insertion_sort(w);
+    my_max(a, b, ([](auto a, auto b) -> auto {return a < b ? b : a; }));
 
-    using cpplab::vector;
-    vector<int> w1;
-    //vector<char> w2;
-    w1.add(10);
-    w1.add(20);
-    w1.add(30);
-    w1.add(40);
-    w1.print();
-    w1.delete_last();
-    w1.print();
-    std::cout << w1.get(1);
+    //zad 2
+    std::vector<int> w{ 23, 1, 10, 5, 2 };
+    std::vector<std::string> w2{ "b","c","a","f","z","h"};
+    insertion_sort(w);
+    insertion_sort(w2);
+
+    //zad 3
+    cpplab::vector<int> w1;
+    w1.push_back(1);
+    w1.push_back(2);
+    w1.push_back(3);
+    w1.push_back(4);
+    w1.push_back(5);
+    w1.push_back(6);
+    w1.push_back(7);
+    w1.push(9, 2);
+    w1.print_vector();
+
+    w1.pop_back();
+    w1.pop_back();
+    w1.print_vector();
+
+    w1.resize(2);
+    w1.print_vector();
+    
+
 }
 
