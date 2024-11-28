6bf16de0d7480e9fd7187adb5e9f2cfef00dad68
Kacper R <126609474+downloadfreeram@users.noreply.github.com>
2024-11-28 16:12:10 +0100

    list4 done

--- cppAdv_lista4/lista4.cpp
+++ cppAdv_lista4/lista4.cpp
@@ -6,16 +6,25 @@ namespace cpplab {
 	class vector {
 	private:
 		T* arr;
-		size_t vector_size = 0;
+		size_t vector_size;
+		size_t vector_capacity;
 		T value_type;
 	public:
 		// default constructor
 		vector() {
 			arr = new T[0];
 			vector_size = 0;
+			vector_capacity = 0;
+		}
+		vector(std::initializer_list<T> list)
+		{
+			for (T& val : list)
+			{
+				push_back(val);
+			}
 		}
 		// copy constructor
-		vector(const vector& other) : arr(new T[other.size()]), vector_size(other.size())
+		vector(const vector& other) : arr(new T[other.size()]), vector_size(other.size()), vector_capacity(other.size() + 2)
 		{
 			std::copy(other.arr, other.arr + other.size(), arr);
 			std::cout << "Copied" << std::endl;
@@ -28,16 +37,18 @@ namespace cpplab {
 				delete[] arr;
 				arr = new int[other.size()];
 				vector_size = other.size();
+				vector_capacity = other.size() + 2;
 				std::copy(other.arr, other.arr + other.size(), arr);
 			}
 			std::cout << "Used copy assignement operator" << std::endl;
 			return *this;
 		}
 		//move constructor
-		vector(vector&& other) : arr(other.arr),vector_size(other.vector_size)
+		vector(vector&& other) : arr(other.arr),vector_size(other.vector_size),vector_capacity(other.vector_capacity)
 		{
 			other.arr = nullptr;
 			other.vector_size = 0;
+			other.vector_capacity = 0;
 			std::cout << "Moved!" << std::endl;
 		}
 		//move assignement operator
@@ -48,8 +59,10 @@ namespace cpplab {
 				delete[] arr;
 				arr = other.arr;
 				vector_size = other.vector_size;
+				vector_capacity = other.vector_capacity;
 				other.arr = nullptr;
 				other.vector_size = 0;
+				other.vector_capacity = 0;
 			}
 			std::cout << "Used move assignement operator" << std::endl;
 			return *this;
@@ -63,63 +76,54 @@ namespace cpplab {
 		}
 		void push_back(const T& value)
 		{
-			T* temp = new T[vector_size + 1];
-			for (size_t i = 0; i < vector_size; i++)
+			if (vector_size == vector_capacity)
 			{
-				temp[i] = arr[i];
+				T* temp = new T[vector_capacity + 2];
+
+				for (size_t i = 0;i < vector_size;i++)
+				{
+					temp[i] = arr[i];
+				}
+				delete[] arr;
+				vector_capacity += 2;
+				arr = temp;
 			}
-			delete[] arr;
-			temp[vector_size] = value;
-			arr = temp;
+			arr[vector_size] = value;
 			vector_size++;
 		}
-		void push(const T& value, size_t index)
+		void resize(int new_capacity)
 		{
-			if (index >= vector_size)
-				throw std::out_of_range("ERROR: Index out of range");
-			T* temp = new T[vector_size];
-			for (size_t i = 0; i < vector_size; i++)
+			if (new_capacity < vector_size)
 			{
-				temp[i] = arr[i];
+				throw std::invalid_argument("ERROR: New value is greater than the vector size");
 			}
-			delete[] arr;
-			temp[index] = value;
-			arr = temp;
-		}
-		void pop_back()
-		{
-			T* temp = new T[vector_size - 1];
-			for (size_t i = 0; i < vector_size - 1; i++)
+			else
 			{
-				temp[i] = arr[i];
+				T* temp = new T[new_capacity];
+				for (size_t i = 0;i < vector_size;i++)
+				{
+					temp[i] = arr[i];
+				}
+				vector_capacity = new_capacity;
+				delete[] arr;
+				arr = temp;
 			}
-			delete[] arr;
-			arr = temp;
-			vector_size--;
 		}
-		void resize(const int new_size)
+		template<typename... args>
+		void emplace_back(args&&... values)
 		{
-			if (new_size >= vector_size)
-				throw std::invalid_argument("ERROR: The resize value is greater than the vector length ");
-			T* temp = new T[new_size];
-			for (size_t i = 0; i < new_size; i++)
+			if (vector_size == vector_capacity)
 			{
-				temp[i] = arr[i];
+				resize(vector_capacity + 2);
 			}
-			delete[] arr;
-			arr = temp;
-			vector_size = new_size;
-		}
-		void emplace_back(const T& value)
-		{
-			
+			arr[vector_size++] = T(std::forward<args>(values)...);
 		}
 		size_t size() const { return vector_size; }
 		void print_vector()
-		{
+		{	
 			for (size_t i = 0; i < vector_size; i++)
 			{
-				std::cout << arr[i] << std::endl;
+				std::cout << arr[i] << " ";
 			}
 			std::cout << std::endl;
 		}
@@ -144,7 +148,21 @@ auto operator*(const U& vec1, const W& vec2)
 	return dotProduct;
 }
 
+class pixel {
+public:
+	pixel() = default;
+	pixel(int red, int green, int blue) : r(red), g(green), b(blue) {}
+
+	friend std::ostream& operator<<(std::ostream& os, const pixel& p)
+	{
+		return os << "[" << p.r << "," << p.g << "," << p.b << "]";
+	}
+private:
+	int r, g, b;
+};
+
 int main() {
+	//zadanie 1
 	cpplab::vector<int> a;
 	cpplab::vector<int> b = a;
 	cpplab::vector<int> c;
@@ -154,6 +172,13 @@ int main() {
 	e = std::move(b);
 
 	a.push_back(3);
+	a.push_back(4);
 
 	a.print_vector();
+
+	//zadanie 2
+	cpplab::vector <pixel> p1;
+	p1.emplace_back(1, 2, 3);
+	p1.emplace_back(4, 5, 6);
+	p1.print_vector();
 }
