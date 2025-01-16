c30e007119798bf65130477d2c51e1df76cc65fe
Kacper <rybkowski84@gmail.com>
2025-01-16 16:47:32 +0100

    Working on lista 7

---
+++ cppAdv_lista7/lista7.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+double scalarOperation(std::vector<double>& v, std::vector<double>& w) {
+	if (v.size() != w.size() || v.size() == 0 || w.size() == 0) {
+		return -1;
+	}
+	else {
+		double sum = 0.0;
+		for (int i = 0;i <= v.size();i++)
+		{
+			sum += v[i] * w[i];
+		}
+		return sum;
+	}
+}
+int main() {
+	std::vector<double> v = { 1.0,2.0,3.0 };
+	std::vector<double> w = { 3.0,5.0,2.0 };
+	std::cout << scalarOperation(v, w) << "\n";
+	return 0;
+}
